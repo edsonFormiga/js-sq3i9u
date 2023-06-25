3900424fe15d734844e1dfae2619c31cbffc22fc
edsonFormiga <137646154+edsonFormiga@users.noreply.github.com>
2023-06-25 15:35:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-sq3i9u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sq3i9u)
---
+++ index.js
@@ -0,0 +1,5 @@
+let idadeDAThays = 53;
+let idadeDoDouglas = 49;
+
+let comparacao1 = idadeDAThays > idadeDoDouglas);
+let comparacao2 = idadeDAThays < idadeDoDouglas);
